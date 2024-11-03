7a4f1d0aa50d1dfef036b09868ea8fa4b782a193
Manisha Reshaveni <manishar@infinite.com>
2024-11-03 18:59:36 +0530

    assignement2

--- Csharp/Assignments/Assignment2/Assignment2/Assignment2/Arrayintoanother.cs
+++ Csharp/Assignments/Assignment2/Assignment2/Assignment2/Arrayintoanother.cs
@@ -12,17 +12,26 @@ namespace Assignment2
         public static void Array()
         {
             int[] Array1 = new int[] { 1, 2, 3, 4, 3 };
-            int[] Array2 = new int[5];
-            int index = 0;
+            int[] Array2 = new int[Array1.Length];
             Console.WriteLine("Array1 elements are:");
-            foreach (int item in Array1)
+            for (int i=0;i<Array1.Length;i++)
+            {          
+                Console.WriteLine(Array1[i]);
+                Array2[i] = Array1[i];
+            }
+            Console.WriteLine("The elements of Array1 are copied into Array2: ");
+            for(int j=0;j<Array2.Length;j++)
+            {
+                Console.WriteLine(Array2[j] + " ");
+            }
+           /* foreach (int item in Array1)
             {
                 Array2[index] = item;
                 Console.WriteLine( item);
                 index++;
             }
             Console.WriteLine("Array2 elements are:");
-            Console.WriteLine(string.Join(" ",Array2));
+            Console.WriteLine(string.Join(" ",Array2));*/
         }
         static void Main(string[] args)
         {
