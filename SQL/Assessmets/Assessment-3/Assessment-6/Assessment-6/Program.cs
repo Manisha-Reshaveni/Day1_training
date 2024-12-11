5aa71c9daeb534c174e35343a97e28904d0f5aca
Manisha Reshaveni <manishar@infinite.com>
2024-12-11 12:14:20 +0530

    Assessment-6

---
+++ SQL/Assessmets/Assessment-3/Assessment-6/Assessment-6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment_6
+{
+    class Program
+    {
+        static SqlConnection con;
+        static SqlCommand cmd;
+
+        public static void Main()
+        {
+            using (SqlConnection con = new SqlConnection("Server=ICS-LT-D244D6BH;initial catalog=Assessments; integrated security=true;"))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand("sp_products", con))
+                {
+
+                    cmd.Parameters.Add(new SqlParameter("@ename", SqlDbType.VarChar, 10)).Value = "manisha";
+                    cmd.Parameters.Add(new SqlParameter("@price", SqlDbType.Int)).Value = 5000;
+                 
+                    SqlParameter param1 = new SqlParameter("@pid", SqlDbType.Int);
+                    param1.Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add(param1);
+                    SqlParameter param2 = new SqlParameter("@discount", SqlDbType.Float);
+                    param2.Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add(param2);
+                    cmd.ExecuteNonQuery();
+                    int product_id = Convert.ToInt32(cmd.Parameters["@pid"].Value);
+                    int discount = Convert.ToInt32(cmd.Parameters["@discount"].Value);
+                    Console.WriteLine($"Product ID : {product_id}  ,  Discount Price : {discount}");
+
+                }
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ SQL/Assessmets/Assessment-3/SQLQueries-3.sql
@@ -0,0 +1,81 @@
+use Assessments
+create table Courses
+(
+CID varchar(20),
+CName varchar(20),
+Start_date date,
+End_date date,
+Fee float
+)
+INSERT INTO Courses VALUES('DN003','DotNet','2018-02-01','2018-02-28',15000),
+('DV004','DataVisualization','2018-03-01','2018-04-15',15000),
+('JA002','AdvancedJava','2018-01-02','2018-01-20',10000),
+('JC001','CoreJava','2018-01-02','2018-01-12',3000)
+select * from Courses
+---1.Create a Function to calculate the course duration for the above relation by receiving start date and end date as input.
+  create or alter function fn_CourseDuration(@start_date date,@end_date date)
+  returns int
+  as
+  begin
+  declare @duration int
+  set @duration = datediff(day,@start_date,@end_date)
+  return @duration
+  end
+
+  select CID,dbo.fn_CourseDuration(Start_date,End_date) Course_Duration_in_days from Courses 
+
+  --Create a trigger to display the Course Name and Start date of the course
+  create or alter trigger triggerCrse
+on courses
+for insert
+as
+begin
+ declare @Cname varchar(20)
+ declare @start_date date
+ select @Cname = CName from courses
+ select @start_date = Start_date from courses
+ select @Cname,@start_date  from Courses
+ end
+ rollback
+
+INSERT INTO Courses Values('DJ005','Java','2018-03-02','2018-04-20',4000)
+select * from Courses
+-------------------------------------------
+--Write a stored Procedure that inserts records in the ProductsDetails table
+
+--Table : ProductsDetails (ProductId, ProductName, Price, DiscountedPrice)
+
+create table Product_Details
+(ProductId int primary key,
+ProductName varchar(20),
+Price float,
+DiscountedPrice as (price-Price*0.10) 
+)
+	insert  into Product_Details values(1,'manisha',5000)
+create or alter  procedure sp_products 
+as
+begin
+declare @ProductName varchar(20),@price float,@productID int 
+	INSERT INTO Products_Details(productname,price) values (@ProductName,@price)
+	 select @productID = case
+	   when max(ProductId) is null then 0
+	   else max(ProductId)
+	   end
+	   from Product_Details
+	set @productID = @productID + 1
+	end
+	exec sp_products
+	select * from Product_Details
+
+	insert  into Product_Details (productname,price) values(5,'ali',2000)
+
+
+
+
+
+ 
+--The procedure should generate the ProductId automatically to insert and should return the generated value to the user
+--Hint(User should not give the ProductId while inserting)
+--Also the Discounted Price Column is a calculated column (Price - 10%)
+ 
+--Test the Procedure using ADO classes to call the Procedure and show the generated ProductId, and Discounted Price
