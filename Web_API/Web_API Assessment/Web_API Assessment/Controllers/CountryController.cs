731736ff3362d392467257c10ba42cebc3f3f574
Manisha Reshaveni <manishar@infinite.com>
2025-01-08 10:45:24 +0530

    WebApi Assessment

---
+++ Web_API/Web_API Assessment/Web_API Assessment/Controllers/CountryController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Web_API_Assessment.Models;
+
+namespace Web_API_Assessment.Controllers
+{
+    [RoutePrefix("api/User")]
+    public class CountryController : ApiController
+    {
+        List<Country> countries = new List<Country>();
+
+        public CountryController()
+        {
+            countries.Add(new Country { ID = 1, CountryName = "USA", Capital = "Washington D.C." });
+            countries.Add(new Country { ID = 2, CountryName = "Canada", Capital = "Ottawa" });
+            countries.Add(new Country { ID = 3, CountryName = "Mexico", Capital = "Mexico City" });
+        }
+
+
+        [HttpGet]
+        [Route("All")]
+        public HttpResponseMessage GetAllPGetAllCountries()
+        {
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, countries);
+            return response;
+        }
+        [HttpGet]
+        [Route("ById")]
+        public IHttpActionResult GetCountryById(int cId)
+        {
+            string cname = countries.Where(c => c.ID == cId).SingleOrDefault()?.CountryName;
+            if (cname == null)
+            {
+                return NotFound();
+            }
+            return Ok(cname);
+        }
+        // POST api/country
+        [HttpPost]
+        [Route("AllPost")]
+        public List<Country> PostAll([FromBody] Country country)
+        {
+            countries.Add(country);
+            return countries;
+        }
+        [HttpPut]
+        [Route("Put")]
+        public IEnumerable<Country> Put(int Cid, [FromUri] Country c)
+        {
+            countries[Cid - 1] = c;
+            return countries;
+        }
+        [HttpDelete]
+        [Route("Delete")]
+        public IEnumerable<Country> Delete(int cid)
+        {
+            countries.RemoveAt(cid - 1);
+            return countries;
+        }
+    }
+}
---
+++ Web_API/Web_API Assessment/Web_API Assessment/Models/Country.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_API_Assessment.Models
+{
+    public class Country
+    {
+//        1. Create a Model called Country under Models Folder.
+//2. Add Properties in the country model as 
+//   ID, CountryName, Capital
+ 
+//3. Create an Empty api controller called Country
+ 
+//4. Perform CRUD operations using either (HTTPResponseMessage/ IHttpActionResult / others) and
+//        consume the api using either  Postman/Swagger
+
+        public int ID { get; set; }
+        public string CountryName { get; set; }
+        public string Capital { get; set; }
+
+    }
+}
